3e1692c35d50804ae63f3e1c09013aa9661a052c
TaitotaloSanteri <santeri.hakoniemi@edu.taitotalo.fi>
2021-01-20 11:21:33 +0200

    Initial commit

---
+++ Assets/Scripts/HighscoreManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighscoreManager : MonoBehaviour
+{
+
+}
+
+public class HighScore
+{
+    public string name;
+    public int score;
+    public string date;
+}
